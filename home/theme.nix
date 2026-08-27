d1c2e09959ae90fa4cd676499e3814e7ff52fd8a
notnikbtw <nikotakovsl@gmail.com>
2026-08-27 23:45:47 +0200

    feat(theme): add Gruvbox GTK/Qt theme

---
+++ home/theme.nix
@@ -0,0 +1,42 @@
+{ pkgs, ... }:
+{
+  home.pointerCursor = {
+    gtk.enable = true;
+    x11.enable = true;
+    name = "Adwaita";
+    package = pkgs.adwaita-icon-theme;
+    size = 24;
+  };
+
+  gtk = {
+    enable = true;
+    theme = {
+      name = "Gruvbox-Dark-BL";
+      package = pkgs.gruvbox-gtk-theme;
+    };
+    iconTheme = {
+      name = "Papirus-Dark";
+      package = pkgs.papirus-icon-theme;
+    };
+    cursorTheme = {
+      name = "Adwaita";
+      package = pkgs.adwaita-icon-theme;
+      size = 24;
+    };
+  };
+
+  qt = {
+    enable = true;
+    platformTheme.name = "gtk3";
+    style.name = "gtk2";
+  };
+
+  dconf.settings = {
+    "org/gnome/desktop/interface" = {
+      gtk-theme = "Gruvbox-Dark-BL";
+      icon-theme = "Papirus-Dark";
+      cursor-theme = "Adwaita";
+      color-scheme = "prefer-dark";
+    };
+  };
+}
