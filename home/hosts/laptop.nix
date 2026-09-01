4d577bef484a529920be6d1863e429fe0002e39a
nik <nikotakovsl@gmail.com>
2026-09-01 22:39:21 +0200

    feat(waybar): separate host-specific waybar configurations for laptop and desktop

--- home/hosts/laptop.nix
+++ home/hosts/laptop.nix
@@ -1,4 +1,7 @@
 { ... }:
 {
   imports = [ ../home.nix ];
+
+  xdg.configFile."waybar/config.jsonc".source = ../waybar/config-laptop.jsonc;
 }
+
--- home/waybar.nix
+++ home/waybar.nix
@@ -1,5 +1,5 @@
 { ... }:
 {
-  xdg.configFile."waybar/config.jsonc".source = ./waybar/config.jsonc;
-  xdg.configFile."waybar/style.css".source    = ./waybar/style.css;
+  xdg.configFile."waybar/style.css".source = ./waybar/style.css;
 }
+
